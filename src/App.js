27f28c745b6d81b43b784ef57c07e2e3ed9a873a
Pataki András <anpataki@gmail.com>
2020-09-21 18:05:45 +0200

    create-react-app, prop-types

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">testing 1-2-3-4</header>
+		</div>
+	);
+}
+
+export default App;
